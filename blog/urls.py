854bed895924ca862bdd172737053c90e5a2d407
stbit <pankovstbit@gmail.com>
2024-05-26 19:20:40 +0400

    base-posts layouts

--- blog/urls.py
+++ blog/urls.py
@@ -6,8 +6,8 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.blog_home, name='blog_home'),  
-    path('<str:slug>/', views.blog_post, name='blog_post'),  
+    path('', views.blog_home, name='blog_home'),
+    path('<str:slug>/', views.blog_post, name='blog_post'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
